e69036e1a17346299d2fbc8d1e6e9c97c997ad7c
doug-kd <31511553+doug-kd@users.noreply.github.com>
2017-11-07 12:12:44 +0000

    Working with a funky Gif

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,15 @@ import {cyan500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -45,18 +54,18 @@ render () {
   return(
     <div>
       <form onSubmit={this.onSubmit}>
-        <input value={this.state.term} onChange={this.onChange} />
-        <RaisedButton primary={true} style={true}>+</RaisedButton>
         <TextField
-      hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
+      value={this.state.term} onChange={this.onChange}
+      hintText="Item 1"
+      floatingLabelText="Add to your list here"
       floatingLabelFixed={true}
     />
+      <RaisedButton type='submit' primary={true} style={true}>+</RaisedButton>
       </form>
       <List items={this.state.items} onComplete={this.onComplete}/>
     </div>
   )
 }
-}
+};
 
 export default App;
